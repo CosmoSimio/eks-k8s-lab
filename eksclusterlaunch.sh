acf7af058fdb93fb539a80f0a697843df121b790
Xolo-itzcuintle <78327139+Xolo-itzcuintle@users.noreply.github.com>
2022-09-18 14:19:22 -0500

    updates

--- eksclusterlaunch.sh
+++ eksclusterlaunch.sh
@@ -1,9 +1,10 @@
 #This bash script contains the command to deploy the EKS cluster
 eksctl create cluster \
---name eks-k8s-lab-noteapp \
---version 1.21 \
+--name eks-k8s-lab-cluster \
+--version 1.23 \
 --region us-east-2 \
 --nodegroup-name ekslab-k8s-nodes \
 --node-type t2.small \
---nodes 2
+--nodes 2 \
+--tags Key=Owner,Value=<INSERT_WORK_EMAIL> Key=Purpose,Value=Lab Key=Name,Value=<yourname>-eks-k8s-lab
 exit

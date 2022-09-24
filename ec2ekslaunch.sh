6a298ffff3015c2b540331587418947d44d7a00d
Xolo-itzcuintle <78327139+Xolo-itzcuintle@users.noreply.github.com>
2022-09-24 12:07:18 -0500

    updated to latest version of files

--- ec2ekslaunch.sh
+++ ec2ekslaunch.sh
@@ -1,4 +1,7 @@
+#!/bin/bash -e
+#This bash script is simply executing the aws CLI command that deploys the Linux EC2 instance which we will dedicate to our EKS lab
 aws ec2 run-instances \
+--name ec2ekslinhost1 \
 --image-id <imageID> \
 --count 1 \
 --instance-type t2.small \
--- prereqmacinstalls.sh
+++ prereqmacinstalls.sh
@@ -1,26 +1,36 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -e
 #Run this if this is your first time using the AWS CLI and/or Homebrew and/or Kubectl
 
+echo "Installing kubectl. . . ."
 #Install kubectl
 curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.23.7/2022-06-29/bin/darwin/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bash_profile
 
+echo "Installing Homebrew. . . ."
 #Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+echo "Installing Weaveworks Homebrew Tap. . . ."
 #Install Weaveworks Homebrew tap tool
 brew tap weaveworks/tap
 
+echo "Installing eksctl. . . ."
 #Install eksctl
 brew install weaveworks/tap/eksctl
 
+echo "Installing tree. . . ."
+#Install tree tool
+brew install tree
+
+echo "Installting helm. . . ."
+#Install Helm
+brew install helm
+
+echo "Installing aws CLI"
 #Install the aws CLI tool
 curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
 sudo installer -pkg ./AWSCLIV2.pkg -target /
 
-#Install tree tool
-brew install tree
 echo "EKS Lab installation complete"
-exit

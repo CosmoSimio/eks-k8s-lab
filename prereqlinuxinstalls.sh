e5f4bb3b7a45be4795d64fc013844a57f2787080
Xolo-itzcuintle <78327139+Xolo-itzcuintle@users.noreply.github.com>
2022-09-24 13:44:35 -0500

    updates

--- prereqlinuxinstalls.sh
+++ prereqlinuxinstalls.sh
@@ -4,19 +4,16 @@
 #This updates your Linux distro
 sudo apt update && sudo apt upgrade -y
 
-
 #Install kubectl
 curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.23.7/2022-06-29/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 
-
 #Install eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 #Move the downloaded ekstl binary to /usr/local/bin
 sudo mv /tmp/eksctl /usr/local/bin
-
 
 #Install helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
